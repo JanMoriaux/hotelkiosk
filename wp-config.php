0662d9f1d88cb24125d775fbf6a76de5729b93f0
Guillaume Vandecasteele <guillaume.vandecasteele@gmail.com>
2017-05-29 22:36:30 +0200

    Updated config

--- wp-config.php
+++ wp-config.php
@@ -23,13 +23,13 @@
 define('DB_NAME', 'wp');
 
 /** MySQL database username */
-define('DB_USER', 'root');
+define('DB_USER', 'wpadmin');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'ikke1961');
+define('DB_PASSWORD', 'pass');
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost:3307');
+define('DB_HOST', 'localhost:3306');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
